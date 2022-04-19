410cfdecdad96e524be6b60f207c745be5dc9d37
Amila K <28174846+amilak86@users.noreply.github.com>
2022-04-20 00:38:07 +0530

    enabled gulp task runner to minify optimize assets

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+const gulp = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+const terser = require('gulp-terser');
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
+
+gulp.task('minify-html', () => {
+  return gulp.src('src/*.html')
+    .pipe(htmlmin({ 
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('minify-styles', (done) => {
+	gulp.src('src/css/*.css')
+		.pipe(cleanCSS())
+		.pipe(rename({suffix: '.min', extname: '.css'}))
+		.pipe(gulp.dest('dist/css'))
+		.on('error', function(err){
+			console.error(err);
+		});		
+
+	done();
+});
+
+gulp.task('uglify-scripts', (done) => {
+	gulp.src('src/js/*.js')
+		.pipe(terser())
+		.pipe(rename({extname: '.min.js'}))
+		.pipe(gulp.dest('dist/js'))
+		.on('error', function(err){
+			console.error(err);
+		});
+
+	done();
+})
+
+
+gulp.task('default', gulp.series('minify-html', 'minify-styles', 'uglify-scripts'));
